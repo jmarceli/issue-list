3318a992b0ed607965b9e3a28933e1c99787f465
Jan Grzegorowski <grzegorowski@gmail.com>
2018-11-25 23:12:29 +0100

    test: remove duplicated test from App

--- src/components/App/index.test.jsx
+++ src/components/App/index.test.jsx
@@ -4,10 +4,7 @@ import { GET_ISSUES_MOCK } from '../../apollo/__mocks__/queries';
 import { render } from '../../test-utils';
 import 'react-testing-library/cleanup-after-each';
 
-test('<App/> rendering', async () => {
+test('<App/> rendering', () => {
   const { getByText } = render(<App classes={{}} />, [GET_ISSUES_MOCK]);
   expect(getByText(/^Latest 20 issues.*$/)).not.toBe(null);
-
-  const loading = getByText('Loading...');
-  expect(loading).not.toBe(null);
 });
