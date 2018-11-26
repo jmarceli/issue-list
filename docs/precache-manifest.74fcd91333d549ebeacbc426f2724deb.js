258a3fd285530287031b92753597670c285134c1
Jan Grzegorowski <grzegorowski@gmail.com>
2018-11-26 08:55:12 +0100

    chore: add GH pages deployment

---
+++ docs/precache-manifest.74fcd91333d549ebeacbc426f2724deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ea3826a879f40c68da0c",
+    "url": "./static/js/main.ea3826a8.chunk.js"
+  },
+  {
+    "revision": "baa7d9fd772d0d57c3c4",
+    "url": "./static/js/1.baa7d9fd.chunk.js"
+  },
+  {
+    "revision": "ea3826a879f40c68da0c",
+    "url": "./static/css/main.6e864013.chunk.css"
+  },
+  {
+    "revision": "8c1ce42c4f29a1595161e7babd932015",
+    "url": "./index.html"
+  }
+];
